b4ec34282bcc3121fb8291f2ecb01c1e1ef3071c
Price Shoemaker <priceshoemaker@Prices-MacBook-Pro.local>
2019-10-09 22:09:50 -0500

    user role basics and flag button (not implemented)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,9 +11,9 @@ export class AppComponent {
 
   constructor() {
     this.articles = [
-      new Article("Angular", "http://angular.io", 3),
-      new Article("Fullstack", "http://fullstack.io", 2),
-      new Article("Angular Homepage", "http://angular.io", 1)
+      new Article("Angular", "http://angular.io", 3, "admin"),
+      new Article("Fullstack", "http://fullstack.io", 2, "user"),
+      new Article("Angular Homepage", "http://angular.io", 1, "user")
     ];
   }
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
